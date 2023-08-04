f350ff11b61cf5516a2f7de0f3aa04144ad7753b
Juan Felipe Urueña Calderón <90998857+juanfuc@users.noreply.github.com>
2023-08-04 12:07:58 -0500

    arreglando paneles

---
+++ docs/_app/immutable/chunks/1.c5f67579.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.f5cb5c93.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2.c45d6e57.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.a1785d87.js";export{t as component};
