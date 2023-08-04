0b59f995cbd0e979dcf2628f806f0d96f53db170
Juan Felipe Urueña Calderón <90998857+juanfuc@users.noreply.github.com>
2023-08-04 15:41:50 -0500

    con acerca

---
+++ docs/_app/immutable/chunks/1.6ceef6e7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.be18d52b.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2.7125c2d5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.1eff21cb.js";export{t as component};
