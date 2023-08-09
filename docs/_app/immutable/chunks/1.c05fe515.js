846f7a062418a9a1ff472f7488307f27792b3f63
Juan Felipe Urueña Calderón <90998857+juanfuc@users.noreply.github.com>
2023-08-09 09:26:10 -0500

    update panel corografia

---
+++ docs/_app/immutable/chunks/1.c05fe515.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.7a64f972.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2.55651125.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.5df380f5.js";export{t as component};
