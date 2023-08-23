436737d84084638a6cbab572cffe62a2a784438f
Juan Felipe Urueña Calderón <90998857+juanfuc@users.noreply.github.com>
2023-08-22 21:59:16 -0500

    update acerca y teoria

---
+++ docs/_app/immutable/chunks/1.69c90404.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ce2e529b.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2.0a2f24de.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.189b2d31.js";export{t as component};
